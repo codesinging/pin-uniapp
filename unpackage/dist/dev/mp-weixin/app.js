aa9be29f99dacdd1b003cc017fbcd63a9c5c9dd9
codesinging <codesinging@gmail.com>
2022-07-21 16:15:40 +0800

    增加 tabs 页面

--- unpackage/dist/dev/mp-weixin/app.js
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports[Symbol.toStringTag] = "Module";
 var common_vendor = require("./common/vendor.js");
 if (!Math) {
-  "./pages/index/index.js";
+  "./pages/home.js";
+  "./pages/category.js";
+  "./pages/cart.js";
+  "./pages/mine.js";
 }
 const _sfc_main = {
   onLaunch: function() {
